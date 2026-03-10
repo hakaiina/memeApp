380307ea71b028b1a3c4cbb54397ea0bce16f65b
Полина Старостина <ruslikkanonnn@gmail.com>
2026-03-10 17:31:01 +0300

    Улучшен дизайн окна регистрации

--- screens/registr_screen.py
+++ screens/registr_screen.py
@@ -1,17 +1,25 @@
 import pygame
 from ui.button import Button
-from config import SCREEN_WIDTH, SCREEN_HEIGHT, BUTTON_SIZE
+from config import SCREEN_WIDTH, SCREEN_HEIGHT, BUTTON_SIZE_REGISTER
 
 
 class RegisterScreen:
     def __init__(self, screen, db, switch_screen_callback):
         self.screen = screen
+
+        self.background = pygame.image.load("assets/images/bg_register.jpg").convert()
+        self.background = pygame.transform.scale(
+            self.background, (SCREEN_WIDTH, SCREEN_HEIGHT)
+        )
+
         self.db = db
         self.switch_screen = switch_screen_callback
 
         pygame.font.init()
         font_path = "assets/fonts/comic.ttf"
-        self.font_text = pygame.font.Font(font_path, 48)
+        self.font_text = pygame.font.Font(font_path, 58)
+        self.font_text_btn = pygame.font.Font(font_path, 28)
+        self.font_fields = pygame.font.Font(font_path, 46)
 
         self.bg_color = (245, 245, 245)
 
@@ -19,23 +27,52 @@ class RegisterScreen:
         self.username = ""
         self.password = ""
 
-        self.active_input = "username"
+        self.active_input = None
+
+        # placeholder
+        self.username_placeholder = "Введите имя"
+        self.password_placeholder = "Введите пароль"
+
+        # cursor
+        self.cursor_visible = True
+        self.cursor_timer = 0
+
+
+        self.username_rect = pygame.Rect(
+            SCREEN_WIDTH // 2 - 200,
+            260,
+            400,
+            60
+        )
+
+        self.password_rect = pygame.Rect(
+            SCREEN_WIDTH // 2 - 200,
+            350,
+            400,
+            60
+        )
+
+        #colors fields
+        self.input_color = (255, 255, 255)
+        self.active_color = (220, 240, 255)
+        self.border_color = (0, 0, 0)
+        self.placeholder_color = (150, 150, 150)
 
         # register button
         self.register_btn = self.create_button()
 
 
     def create_button(self):
-        normal = pygame.Surface(BUTTON_SIZE)
-        normal.fill((100, 200, 100))
+        normal = pygame.Surface(BUTTON_SIZE_REGISTER)
+        normal.fill((120, 200, 120))
 
-        hover = pygame.Surface(BUTTON_SIZE)
-        hover.fill((80, 180, 80))
+        hover = pygame.Surface(BUTTON_SIZE_REGISTER)
+        hover.fill((100, 180, 100))
 
         return Button(
             image=normal,
             hover_image=hover,
-            pos=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 150),
+            pos=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 200),
             callback=self.register_user
         )
 
@@ -50,21 +87,32 @@ class RegisterScreen:
 
 
     def handle_event(self, event):
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_TAB:
-                # switch fields
-                self.active_input = (
-                    "password" if self.active_input == "username" else "username"
-                )
-            elif event.key == pygame.K_BACKSPACE:
+        # select field by click
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.username_rect.collidepoint(event.pos):
+                self.active_input = "username"
+            elif self.password_rect.collidepoint(event.pos):
+                self.active_input = "password"
+            else:
+                self.active_input = None
+
+        # input text
+        if event.type == pygame.KEYDOWN and self.active_input:
+
+            if event.key == pygame.K_BACKSPACE:
+
                 if self.active_input == "username":
                     self.username = self.username[:-1]
-                else:
+
+                if self.active_input == "password":
                     self.password = self.password[:-1]
+
             else:
+
                 if self.active_input == "username":
                     self.username += event.unicode
-                else:
+
+                if self.active_input == "password":
                     self.password += event.unicode
 
         self.register_btn.handle_event(event)
@@ -73,27 +121,89 @@ class RegisterScreen:
     def update(self):
         self.register_btn.update()
 
+        self.cursor_timer += 1
+
+        if self.cursor_timer > 30:
+            self.cursor_visible = not self.cursor_visible
+            self.cursor_timer = 0
+
 
     def draw(self):
-        self.screen.fill(self.bg_color)
+
+        self.screen.blit(self.background, (0, 0))
 
         title = self.font_text.render("Регистрация", True, (0, 0, 0))
-        self.screen.blit(title, (SCREEN_WIDTH // 2 - 100, 100))
+        title_rect = title.get_rect(center=(SCREEN_WIDTH // 2, 150))
+        self.screen.blit(title, title_rect)
 
-        username_text = self.font_text.render(
-            f"Имя: {self.username}",
-            True,
-            (0, 0, 0)
+        # username field
+        color = self.active_color if self.active_input == "username" else self.input_color
+
+        pygame.draw.rect(self.screen, color, self.username_rect)
+        pygame.draw.rect(self.screen, self.border_color, self.username_rect, 2)
+
+        username_text = self.username
+
+        if self.active_input == "username" and self.cursor_visible:
+            username_text += "|"
+
+        if self.username == "" and self.active_input != "username":
+            surface = self.font_fields.render(
+                self.username_placeholder,
+                True,
+                self.placeholder_color
+            )
+        else:
+            surface = self.font_fields.render(
+                username_text,
+                True,
+                (0, 0, 0)
+            )
+        rect = surface.get_rect(
+            midleft=(self.username_rect.x + 10, self.username_rect.centery - 5)
         )
 
-        password_hidden = "*" * len(self.password)
-        password_text = self.font_text.render(
-            f"Пароль: {password_hidden}",
-            True,
-            (0, 0, 0)
+        self.screen.blit(surface, rect)
+
+        # password field
+        color = self.active_color if self.active_input == "password" else self.input_color
+
+        pygame.draw.rect(self.screen, color, self.password_rect)
+        pygame.draw.rect(self.screen, self.border_color, self.password_rect, 2)
+
+        hidden_password = "*" * len(self.password)
+
+        if self.active_input == "password" and self.cursor_visible:
+            hidden_password += "|"
+
+        if self.password == "" and self.active_input != "password":
+
+            surface = self.font_fields.render(
+                self.password_placeholder,
+                True,
+                self.placeholder_color
+            )
+        else:
+            surface = self.font_fields.render(
+                hidden_password,
+                True,
+                (0, 0, 0)
+            )
+
+        rect = surface.get_rect(
+            midleft=(self.password_rect.x + 10, self.password_rect.centery - 5)
         )
+        self.screen.blit(surface, rect)
 
-        self.screen.blit(username_text, (SCREEN_WIDTH // 2 - 200, 250))
-        self.screen.blit(password_text, (SCREEN_WIDTH // 2 - 200, 320))
-
+        # button
         self.register_btn.draw(self.screen)
+
+        btn_text = self.font_text_btn.render(
+            "Зарегистрироваться",
+            True,
+            (0, 0, 0)
+        )
+        btn_rect = btn_text.get_rect(
+            center=self.register_btn.rect.center
+        )
+        self.screen.blit(btn_text, btn_rect)
